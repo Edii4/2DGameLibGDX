d90a50d57f9f0ca72ad24b3604e3f745885f3dd7
Edii4 <edvardkollar@gmail.com>
2024-10-21 18:36:55 +0200

    Units done

---
+++ core/src/main/java/com/edvard/game/units/Gryff.java
@@ -0,0 +1,8 @@
+package com.edvard.game.units;
+
+public class Gryff extends Unit {
+
+    public Gryff(int quantity, int damage, int hp, int defense) {
+        super(quantity, damage, hp, defense);
+    }
+}
