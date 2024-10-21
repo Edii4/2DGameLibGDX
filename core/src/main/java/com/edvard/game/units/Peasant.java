e2a2e7917fd38faca90b5ad2bda1fa8cc98f337a
Edii4 <edvardkollar@gmail.com>
2024-10-21 19:15:19 +0200

    Units extended

--- core/src/main/java/com/edvard/game/units/Peasant.java
+++ core/src/main/java/com/edvard/game/units/Peasant.java
@@ -1,7 +1,7 @@
 package com.edvard.game.units;
 
 public class Peasant extends Unit{
-    public Peasant(int quantity, int damage, int hp, int defense) {
-        super(quantity, damage, hp, defense);
+    public Peasant(boolean heroUnit, int quantity, int damage, int hp, int defense) {
+        super(heroUnit, quantity, damage, hp, defense);
     }
 }
--- core/src/main/java/com/edvard/game/units/Wizard.java
+++ core/src/main/java/com/edvard/game/units/Wizard.java
@@ -1,7 +1,7 @@
 package com.edvard.game.units;
 
 public class Wizard extends Unit {
-    public Wizard(int quantity, int damage, int hp, int defense) {
-        super(quantity, damage, hp, defense);
+    public Wizard(boolean heroUnit, int quantity, int damage, int hp, int defense) {
+        super(heroUnit, quantity, damage, hp, defense);
     }
 }
