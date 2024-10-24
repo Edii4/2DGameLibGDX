af0ac39eaffd463f65f581d37f04ebf903e00919
Edii4 <edvardkollar@gmail.com>
2024-10-24 18:02:18 +0200

    Start button added

--- core/src/main/java/com/edvard/game/screens/FightScreen.java
+++ core/src/main/java/com/edvard/game/screens/FightScreen.java
@@ -34,6 +34,8 @@ public class FightScreen implements Screen {
     private final int MAX_ROW = 10;
     private final int MAX_COL = 10;
 
+    ImageButton startButton;
+
     boolean isPeasantPlaced = false;
     boolean isArcherPlaced = false;
     boolean isWarriorPlaced = false;
@@ -93,6 +95,7 @@ public class FightScreen implements Screen {
         heroPeasantQuantity = new Label(String.valueOf(enemyPeasant.getQuantity()), new Label.LabelStyle(new BitmapFont(), Color.BLACK));
         heroPeasantQuantity.setPosition(buttons[row][8].getX() + 32, buttons[row][8].getY());
         stage.addActor(heroPeasantQuantity);
+        buttons[row][8].setName("enemyPeasant");
 
         row = (int)(Math.random() * 5);
 
@@ -100,6 +103,7 @@ public class FightScreen implements Screen {
         enemyArcherQuantity = new Label(String.valueOf(enemyArcher.getQuantity()), new Label.LabelStyle(new BitmapFont(), Color.BLACK));
         enemyArcherQuantity.setPosition(buttons[row][9].getX() + 32, buttons[row][9].getY());
         stage.addActor(enemyArcherQuantity);
+        buttons[row][9].setName("enemyArcher");
 
         row = (int)(Math.random() * (MAX_ROW - 7) + 7);
 
@@ -107,6 +111,7 @@ public class FightScreen implements Screen {
         enemyWarriorQuantity = new Label(String.valueOf(enemyWarrior.getQuantity()), new Label.LabelStyle(new BitmapFont(), Color.BLACK));
         enemyWarriorQuantity.setPosition(buttons[row][8].getX() + 32, buttons[row][8].getY());
         stage.addActor(enemyWarriorQuantity);
+        buttons[row][8].setName("enemyWarrior");
 
         row = (int)(Math.random() * (MAX_ROW - 5) + 5);
 
@@ -114,6 +119,7 @@ public class FightScreen implements Screen {
         enemyWizardQuantity = new Label(String.valueOf(enemyWizard.getQuantity()), new Label.LabelStyle(new BitmapFont(), Color.BLACK));
         enemyWizardQuantity.setPosition(buttons[row][9].getX() + 10, buttons[row][9].getY());
         stage.addActor(enemyWizardQuantity);
+        buttons[row][9].setName("enemyWizard");
 
         row = (int)(Math.random() * (7 - 3) + 3);
 
@@ -121,6 +127,7 @@ public class FightScreen implements Screen {
         enemyGryffQuantity = new Label(String.valueOf(enemyGryff.getQuantity()), new Label.LabelStyle(new BitmapFont(), Color.BLACK));
         enemyGryffQuantity.setPosition(buttons[row][8].getX() + 7, buttons[row][8].getY() + 45);
         stage.addActor(enemyGryffQuantity);
+        buttons[row][8].setName("enemyGryff");
 
         for(int i = 0; i < MAX_ROW; i++) {
             for(int j = 0; j < 2; j++) {
@@ -167,6 +174,11 @@ public class FightScreen implements Screen {
                             heroGryffQuantity.setPosition(buttons[finalI][finalJ].getX() + 7, buttons[finalI][finalJ].getY() + 45);
                             stage.addActor(heroGryffQuantity);
                             buttons[finalI][finalJ].setName("heroGryff");
+
+                            startButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("buttons/play01.png")))));
+                            startButton.setSize(158, 68.4f);
+                            startButton.setPosition(Gdx.graphics.getWidth() / 2 - startButton.getWidth() / 2, Gdx.graphics.getHeight() / 8 - startButton.getHeight() / 2);
+                            stage.addActor(startButton);
                         }
                     }
                 });
